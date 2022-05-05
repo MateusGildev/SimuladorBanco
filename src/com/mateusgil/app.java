0beef8ab240a26cfbf20dda8b1569dc7caeb0b4b
MateusGildev <63979759+MateusGildev@users.noreply.github.com>
2022-05-05 16:29:42 -0300

    Add files via upload

---
+++ src/com/mateusgil/app.java
@@ -0,0 +1,12 @@
+package com.mateusgil;
+
+import javax.swing.table.TableRowSorter;
+
+public class app {
+    public static void main(String[] args) {
+        Banco c1 = new Banco();
+        while (true){
+            c1.abrirConta();
+        }
+    }
+}
